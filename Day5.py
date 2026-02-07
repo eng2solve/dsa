6579b3d4d95cf6099d621491f455e3cca3982466
eng2solve <durgesh71199@gmail.com>
2026-02-07 09:32:48 +0530

    moed files to seperate folder

--- Day5.py
+++ Day5.py
@@ -14,13 +14,13 @@ s1="abcd"
 s2="bcada"
 print(sorted(list(s1))==sorted(list(s2)))
 
-# # Read file and count lines
-file=open("demo.txt","r") # with open is use bcs it automatically close thefile and have error handeling
+# # Read file and count lines files/demo.txt
+file=open("files/demo.txt","r") # with open is use bcs it automatically close thefile and have error handeling
 lines=sum(1 for line in file) #if I write file.read() it will read the content of the file and will give the total word+space in file
 print(lines)
 
 # # Count word frequency from file
-with open("demo.txt",'r') as file:
+with open("files/demo.txt",'r') as file:
     freq=dict()
     for line in file:
         line=line.strip()
@@ -34,26 +34,26 @@ with open("demo.txt",'r') as file:
     print(freq)
             
 # # Write user input to file
-with open("demo.txt","w") as fp:
+with open("files/demo.txt","w") as fp:
     fp.write("This will remove everthing and write a new \n")
     
 # # Append logs to file
-with open("demo.txt","a") as destinationFile,  open("log.txt","r")as log:
+with open("files/demo.txt","a") as destinationFile,  open("files/log.txt","r")as log:
     for lines in log:
         destinationFile.write(lines)
 
 # Remove blank lines from file
 with open(
-    "demo.txt", 'r') as readFile, open(
-        'cleaned.txt', 'w') as writeFile:
+    "files/demo.txt", 'r') as readFile, open(
+        'files/cleaned.txt', 'w') as writeFile:
    for line in readFile:
         if line.strip():
             writeFile.write(line)
 
 # CSV file reading basics
-import pandas as pd
-df=pd.read_csv("path")
-print(df)
+# import pandas as pd
+# df=pd.read_csv("path")
+# print(df)
 
 # Learn to use pandas (basics) read a csv file, write to a csv file
 # Custom map() implementation
